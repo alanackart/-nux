6f463958c453792310f641a0a24a302a4a86b092
ChaoF <alanackart@gmail.com>
2019-05-28 00:43:08 +0800

    Delete getAllWarning.sh

--- getAllWarning.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    #echo "Text read from file: $line"
-    if [[ $line = *"warning"* ]]; then
-    	  echo $line >> warning.txt
-	fi
-done < "$1"
